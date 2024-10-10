e28035bcf435e54bdfe6398188d9bba0f4a50ceb
thiagopdo <thiago.falconer@gmail.com>
2024-10-10 18:08:50 -0300

    feat: added Sonner as toast provider

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -20,9 +21,23 @@ export default function SignIn() {
   } = useForm<SignInForm>();
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data);
+    try {
+      console.log(data);
 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      toast.success(
+        "Enviamos um e-mail para você com as instruções de acesso",
+        {
+          action: {
+            label: "Reenviar e-mail",
+            onClick: () => handleSignIn(data),
+          },
+        },
+      );
+    } catch {
+      toast.error("Credenciais inválidas");
+    }
   }
 
   return (
