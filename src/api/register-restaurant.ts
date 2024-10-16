9025290b77c011464b267638982feb9671034b5b
thiagopdo <thiago.falconer@gmail.com>
2024-10-16 18:36:36 -0300

    feat: add register restaurant api

---
+++ src/api/register-restaurant.ts
@@ -0,0 +1,22 @@
+import { api } from "@/lib/axios";
+
+export interface RegisterRestaurantBody {
+  restaurantName: string;
+  managerName: string;
+  phone: string;
+  email: string;
+}
+
+export async function registerRestaurant({
+  email,
+  managerName,
+  phone,
+  restaurantName,
+}: RegisterRestaurantBody) {
+  await api.post("/restaurants", {
+    email,
+    managerName,
+    phone,
+    restaurantName,
+  });
+}
