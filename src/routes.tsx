394d488d30090ef99297f97106e1ed9c321eead8
thiagopdo <thiago.falconer@gmail.com>
2024-11-01 17:46:47 -0300

    added: error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,17 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
-import NotFound from "./pages/404";
 import Dashboard from "./pages/app/dashboard/dashboard";
 import Orders from "./pages/app/orders/orders";
 import SignIn from "./pages/auth/sign-in";
 import SingUp from "./pages/auth/sign-up";
+import Error from "./pages/error";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Dashboard /> },
       { path: "/orders", element: <Orders /> },
