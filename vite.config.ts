283f1821b2a8d9d81e3657f215af2bc80619b7e7
thiagopdo <thiago.falconer@gmail.com>
2024-11-01 18:06:36 -0300

    added vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
+import { UserConfig } from "vite";
+import type { InlineConfig } from "vitest/node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +12,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+  test: {
+    globals: true,
+  },
+} as UserConfig & { test: InlineConfig });
